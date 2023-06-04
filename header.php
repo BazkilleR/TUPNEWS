6e7b445a5a0fea1a23599f4116e62de9fd59dce5
BazkilleR <ohm.ratchaphon@gmail.com>
2023-06-04 17:30:25 +0700

    add search system

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
                 <li><a href="tcas67.php">TCAS67</a></li>
                 <li><a href="contact.php">ติดต่อ</a></li>
                 <li><a href="admin.php">ADMIN</a></li>
-                <button type="submit"><a href=""><i class="fa fa-search"></i></a></button>
+                <button type="submit"><a href="search.php"><i class="fa fa-search"></i></a></button>
             </ul>
         </div>
     </nav>
---
+++ search.php
@@ -0,0 +1,50 @@
+<?php
+        $output = "";
+
+        if (isset($_POST['input'])) {
+            $input = $_POST['input'];
+
+            if (!empty($input)) {
+                require 'server.php';
+                mysqli_query($conn, 'Use tup_news;');
+
+                $query = "SELECT * FROM news WHERE topic LIKE '%$input%'";
+                $result = mysqli_query($conn, $query);
+
+                $output .= "<h1>RESULT of '" . $input . "'</h1>";
+
+                if (mysqli_num_rows($result) < 1) {
+                    $output .= "<p>No Data</p>";
+                } else {
+                    while ($dbarr = mysqli_fetch_array($result)) {
+                        $output .= "<li><a>" . $dbarr['topic'] . "</a>&nbsp-----&nbsp" . $dbarr['date'] . "</li>";
+                    }
+                }
+            }
+        }
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Search</title>
+</head>
+
+<body>
+    <?php require 'header.php' ?>
+    <form name="search" method="post">
+        <input type="text" placeholder="Search.." name="input">
+        <button type="submit"><a href="#"><i class="fa fa-search"></i></a></button>
+    </form>
+    <ol>
+        <?php echo $output; ?>
+    </ol>
+</body>
+
+</html>

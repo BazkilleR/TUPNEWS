f061291187287f51e4c8ed9abc0bc38d83a66363
BazkilleR <ohm.ratchaphon@gmail.com>
2023-06-06 22:49:07 +0700

    ADD update img (not complete #2)

--- main/update_news.php
+++ main/update_news.php
@@ -1,3 +1,45 @@
+<?php
+// connect database
+require 'server.php';
+mysqli_query($conn, "USE tup_news");
+
+$output = '';
+
+// check if user doesnt type id
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+
+    $query = "SELECT * FROM news where id='$id'";
+    $result = mysqli_query($conn, $query);
+    $dbarr = mysqli_fetch_array($result);
+
+    $output .= "
+    <form action='update_news_db.php?id=" . $id . "' method='post' enctype='multipart/form-data'>
+        <p>หัวข้อ</p>
+        <input type='text' name='topic' value='" . $dbarr['topic'] . "'><br>
+        <p>คำอธิบาย</p>
+        <input type='text' name='descr' value='" . $dbarr['descr'] . "'><br>
+        <p>เนื้อหา</p>
+        <textarea name='content' cols='30' rows='10'>" . $dbarr['content'] . "</textarea><br>
+        <label for='level'>สำคัญหรือไม่?</label>
+        <input type='radio' name='level' value='1'>
+        <label for='yes'>ใช่</label>
+        <input type='radio' name='level' checked=true value='0'>
+        <label for='no'>ไม่ใช่</label><br>
+        <label for='category'>หมวดหมู่</label>
+        <select name='category' selected=" . $dbarr['category'] . ">
+            <option value='morning'>Morning</option>
+            <option value='camp'>Camp</option>
+            <option value='tcas67'>TCAS67</option>
+            <option value='schedule'>Schedule</option>
+        </select><br>
+        <label for='img'>อัปโหลดรูปภาพ</label>
+        <input type='file' name='img' value='" . $dbarr['img'] . "'><br>
+        <input type='submit' name='submit' value='edit'>
+    </form>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,40 +53,15 @@
 </head>
 
 <body>
-    <?php require ('header.php'); ?>
+    <?php require('header.php'); ?>
     <h1>UPDATE</h1>
-    <form action="update_news_db.php" method="post">
-        <!-- select id -->
+    <!-- select id -->
+    <form action="" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"" method="post">
         <input type="number" name="id" placeholder="select id"><br>
-        <!-- topic -->
-        <p>หัวข้อ</p>
-        <input type="text" name="topic"><br>
-        <!-- descr -->
-        <p>คำอธิบาย</p>
-        <input type="text" name="descr"><br>
-        <!-- content -->
-        <p>เนื้อหา</p>
-        <textarea name="content" cols="30" rows="10"></textarea><br>
-        <!-- level -->
-        <label for="level">สำคัญหรือไม่?</label>
-        <input type="radio" name="level" value="1">
-        <label for="yes">ใช่</label>
-        <input type="radio" name="level" value="0">
-        <label for="no">ไม่ใช่</label><br>
-        <!-- category -->
-        <label for="category">หมวดหมู่</label>
-        <select name="category">
-            <option value="morning">Moring</option>
-            <option value="camp">Camp</option>
-            <option value="tcas67">TCAS67</option>
-            <option value="schedule">Schedule</option>
-        </select><br>
-        <!-- photo -->
-        <label for="img">อัปโหลดรูปภาพ</label>
-        <input type="file" name="img" accept="image/jpeg. image/jpg, image/png"><br>
-        <!-- submit -->
-        <input type="submit" name="submit" value="edit">
+        <input type="submit" value="เลือก">
     </form>
+    <br>
+    <?php echo $output ?>
 </body>
 
 </html>
--- main/update_news_db.php
+++ main/update_news_db.php
@@ -4,44 +4,60 @@ require('server.php');
 mysqli_query($conn, 'Use tup_news;');
 
 // get input
-$id = $_POST['id'];
-$topic = $_POST['topic'];
-$descr = $_POST['descr'];
-$content = $_POST['content'];
-$category = $_POST['category'];
-$level = $_POST['level'];
+$id = $_GET['id'];
+$topic = $_POST['topic'] ?? NULL;
+$descr = $_POST['descr'] ?? NULL;
+$content = $_POST['content'] ?? NULL;
+$category = $_POST['category'] ?? NULL;
+$level = $_POST['level'] ?? NULL;
 
 // get img info
-$fileName = basename($_FILES['img']['name']);
+$fileName = basename($_FILES['img']['name']) ?? NULL;
 $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
 
-if (!empty($id)) {
-    if (!empty($img)){
-        if ($fileType == 'jpg' || $fileType == 'jpeg' || $fileType == 'png') {
-            $target_dir = "img/";
+if (!empty($id)) { // check id input
+    if (!empty($img)) { // check img input
+        if ($fileType == 'jpg' || $fileType == 'jpeg' || $fileType == 'png') { // check img extension
 
             // delete old img file
-            unlink()
+            $query = "SELECT img FROM news WHERE id='$id'";
+            $del_img = mysqli_query($conn, $query);
+            $img_path = mysqli_fetch_array($del_img);
+            $del_img = unlink($img_path['img']);
 
-            // save image file in img dir & delete old file
-            
-            $fileImg = $target_dir . $fileName; 
+            // save image file in img dir
+            $target_dir = "img/";
+            $fileImg = $target_dir . $fileName;
             move_uploaded_file($_FILES['img']['tmp_name'], $fileImg);
         }
     }
-    
-    $query = mysqli_query($conn, "UPDATE news SET topic='$topic', descr='$descr', content='$content', category='$category', level='$level', date=NOW(), img='$img' WHERE id='$id'");
+
+    $target_dir = "img/";
+    $fileImg = $target_dir . $fileName;
+
+    // update data
+    $query =    "UPDATE news SET 
+                topic='$topic' ,
+                descr='$descr' ,
+                content='$content' ,
+                category='$category' ,
+                level='$level' ,
+                date=NOW() ,
+                img='$fileImg'
+                WHERE id='$id' ";
+
+    $result = mysqli_query($conn, $query);
+    mysqli_close($conn);
 
     // check update status
-    if ($query) {
-        echo 'success<br>';
+    if ($result) {
+        echo 'Upadate successfully<br>';
         echo "<a href='index.php'>HOME</a><br>";
         echo "<a href='admin.php'>ADMIN</a><br>";
     } else {
-        echo 'fail';
+        echo 'Upload fail';
     }
 } else {
-    echo 'Please select id.';
+    echo 'Please select id';
 }
-mysqli_close($conn);
 ?>

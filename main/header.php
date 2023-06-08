aa5d07c4214b67f4e42fa01706c6e752fe9ec2c6
BazkilleR <ohm.ratchaphon@gmail.com>
2023-06-08 19:57:50 +0700

    ADD contact.html

--- main/header.php
+++ main/header.php
@@ -9,7 +9,7 @@
                 <li><a href="morning_news.php">หน้าเสาธง</a></li>
                 <li><a href="camp_news.php">ค่าย</a></li>
                 <li><a href="tcas67_news.php">TCAS67</a></li>
-                <li><a href="contact.php">ติดต่อ</a></li>
+                <li><a href="contact.html">ติดต่อ</a></li>
                 <li><a href="admin.php">แอดมิน</a></li>
                 <button type="submit"><a href="search.php"><i class="fa fa-search"></i></a></button>
             </ul>

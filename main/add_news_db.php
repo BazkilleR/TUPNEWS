a78a573d505a16d0a06b19f2b6ee4e807801dee3
Rittigrai <Rittigrai.tp@gmail.com>
2023-06-09 10:48:49 +0700

    optimize add_data_db.php

--- main/add_news_db.php
+++ main/add_news_db.php
@@ -1,42 +1,63 @@
 <?php
-    // connect database
-    require('server.php');
+// Connect to the database
+require('server.php');
 
-    // get input
+// Check if all input fields are set
+$requiredFields = ['topic', 'descr', 'content', 'category', 'level'];
+$isValidInput = true;
+
+foreach ($requiredFields as $field) {
+    if (!isset($_POST[$field])) {
+        $isValidInput = false;
+        break;
+    }
+}
+
+if ($isValidInput) {
     $topic = $_POST['topic'];
     $descr = $_POST['descr'];
     $content = $_POST['content'];
     $category = $_POST['category'];
     $level = $_POST['level'];
 
-    // get img info
-    $fileName = basename($_FILES['img']['name']); 
-    $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+    // Check if the image file is set
+    if (isset($_FILES['img']) && $_FILES['img']['error'] === UPLOAD_ERR_OK) {
+        $fileName = $_FILES['img']['name'];
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
 
-    // check NULL input
-    if (!empty($topic) || !empty($descr) || !empty($content) || !empty($category) || !empty($level) || !empty($fileName)) {
-        // check img type
-        if ($fileType == 'jpg' || $fileType == 'jpeg' || $fileType == 'png') {
-
-            // save image file in img dir
-            $target_dir = "img/";
-            $fileImg = $target_dir . $fileName; 
-            move_uploaded_file($_FILES['img']['tmp_name'], $fileImg);
-
-            // add input to database
-            $query = "  INSERT INTO news (id,topic,descr,content,category,level,UploadDate,img)
-                        VALUES (NULL,'$topic','$descr','$content','$category','$level',NOW(),'$fileImg')";
-            $result = mysqli_query($conn, $query);
+        // Check if the file type is allowed
+        $allowedTypes = ['jpg', 'jpeg', 'png'];
+        if (in_array($fileType, $allowedTypes)) {
+            // Save the image file
+            $targetDir = 'img/';
+            $fileImg = $targetDir . $fileName;
+            $fileTemp = $_FILES['img']['tmp_name'];
             
-            if ($result) {
-                echo 'Add data successfully';
+            if (move_uploaded_file($fileTemp, $fileImg)) {
+                // Insert data into the database using prepared statements
+                $query = "INSERT INTO news (topic, descr, content, category, level, UploadDate, img) VALUES (?, ?, ?, ?, ?, NOW(), ?)";
+                $stmt = mysqli_prepare($conn, $query);
+                mysqli_stmt_bind_param($stmt, 'ssssss', $topic, $descr, $content, $category, $level, $fileImg);
+                $result = mysqli_stmt_execute($stmt);
+
+                if ($result) {
+                    echo 'Add data successfully';
+                } else {
+                    echo 'Add data failure';
+                }
             } else {
-                echo 'add data failure';
+                echo 'Failed to move uploaded file.';
             }
         } else {
-            echo "Sorry, only JPG, JPEG & PNG files are allowed.";
+            echo 'Sorry, only JPG, JPEG & PNG files are allowed.';
         }
     } else {
-        echo "Please type all input";
+        echo 'Please select an image file.';
     }
+} else {
+    echo 'Please fill in all input fields.';
+}
+
+// Disconnect from the database
+mysqli_close($conn);
 ?>

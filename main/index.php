b6591e8573727a7127ee0d846f3e30f0596881b8
BazkilleR <ohm.ratchaphon@gmail.com>
2023-06-10 21:57:08 +0700

    modify frontend

--- main/index.php
+++ main/index.php
@@ -15,43 +15,90 @@
     <?php require('header.php'); ?>
     <div class="grid-container">
         <section class="left">
-            <?php
-            // connect database
-            include('server.php');
-            mysqli_query($conn, 'Use tup_news');
+            <div class="news-title">
+                <h1>ข่าวสาร</h1>
+            </div>
+            <div class="news-container">
+                <?php
+                // connect database
+                include('server.php');
+                mysqli_query($conn, 'Use tup_news');
 
-            $query = mysqli_query($conn, "SELECT * FROM news ORDER BY UploadDate DESC;");
-            // loop get data
-            while ($dbarr = mysqli_fetch_array($query)) {
-                $id = $dbarr['id'];
-                $topic = $dbarr['topic'];
-                $content = $dbarr['content'];
-                $UploadDate = $dbarr['UploadDate'];
-                $img = $dbarr['img'];
-                $category = $dbarr['category'];
-            ?>
-                <div class="box">
-                    <div class="img">
-                        <img src="<?php echo $img; ?>">
+                $query = mysqli_query($conn, "SELECT * FROM news ORDER BY UploadDate DESC;");
+                // loop get data
+                while ($dbarr = mysqli_fetch_array($query)) {
+                    $id = $dbarr['id'];
+                    $topic = $dbarr['topic'];
+                    $content = $dbarr['content'];
+                    $UploadDate = $dbarr['UploadDate'];
+                    $img = $dbarr['img'];
+                    $category = $dbarr['category'];
+                ?>
+                    <div class="box">
+                        <div class="img">
+                            <img src="<?php echo $img; ?>">
+                        </div>
+                        <div class="topic">
+                            <h3><?php echo "<a href='show_detail.php?id=" . $id . "'>" . $topic . "</a>" ?></h3>
+                        </div>
+                        <div class="category-date">
+                            <p class="category"><?php echo $category ?></>
+                            <p class="date"><?php echo $UploadDate ?></p>
+                        </div>
                     </div>
-                    <div class="topic">
-                        <h1 class="topic"><?php echo "<a href='show_detail.php?id=" . $id . "'>" . $topic . "</a>" ?></h1>
-                    </div>
-                    <div class="category-date">
-                        <p class="category"><?php echo $category ?></>
-                        <p class="date"><?php echo $UploadDate ?></p>
-                    </div>
-                </div>
-            <?php
-            } // end loop
-            mysqli_close($conn);
-            ?>
+                <?php
+                } // end loop
+                mysqli_close($conn);
+                ?>
+            </div>
         </section>
         <section class="right">
-            <ul>
-                <li><a href="#">กำหนดการ</a></li>
-                <li><a href="#">เอกสาร</a></li>
-            </ul>
+            <div class="news-title">
+                <h1>ข่าวสำคัญ</h1>
+            </div>
+            <div class="lv1-news-container">
+                <div class="box">
+                    <div class="img">
+                        <img src="https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                    </div>
+                    <div class="content">
+                        <div class="topic">
+                            <h3>"นิพนธ์" ปชป. เตือน ปม ประชามติแยกดินแดน ขัดรัฐธรรมนูญ หวั่นรุนแรงเพิ่ม</h3>
+                        </div>
+                        <div class="category-date">
+                            <p class="category">Morning</>
+                            <p class="date">2023-06-08</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="img">
+                        <img src="https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                    </div>
+                    <div class="content">
+                        <div class="topic">
+                            <h3>"นิพนธ์" ปชป. เตือน ปม ประชามติแยกดินแดน ขัดรัฐธรรมนูญ หวั่นรุนแรงเพิ่ม</h3>
+                        </div>
+                        <div class="category-date">
+                            <p class="category">Morning</>
+                            <p class="date">2023-06-08</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="img">
+                        <img src="https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                    </div>
+                    <div class="content">
+                        <div class="topic">
+                            <h3>"นิพนธ์" ปชป. เตือน ปม ประชามติแยกดินแดน ขัดรัฐธรรมนูญ หวั่นรุนแรงเพิ่ม</h3>
+                        </div>
+                        <div class="category-date">
+                            <p class="category">Morning</>
+                            <p class="date">2023-06-08</p>
+                        </div>
+                    </div>
+                </div>
         </section>
     </div>
 </body>

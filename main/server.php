3a52c7e57927ebb0fd2936bb9959bbb36327ed70
BazkilleR <ohm.ratchaphon@gmail.com>
2023-06-10 23:29:28 +0700

    modify server.php

--- main/server.php
+++ main/server.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "tup_news";
+$servername = "id20892323_tup_news";
+$username = "id20892323_tupnews_database";
+$password = "%d9vBjCv1)zUNGBc";
+$database = "id20892323_tup_news";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 ?>

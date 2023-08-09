f8df58db9e1c65d859d0ebc10cdb8e8075d50b37
BazkilleR <ohm.ratchaphon@gmail.com>
2023-08-09 23:23:11 +0700

    fix height not equal in tcas67_news.php

--- main/tcas67_news.php
+++ main/tcas67_news.php
@@ -7,13 +7,13 @@
         <div class="container-fluid">
             <div class="mb-5 Timer">
                 <div class="card text-center round-5">
-                    <h3 class="card-header" style="background-color: #F07C1C; color: whitesmoke; border-radius: 0.5em 0.5em 0 0; ">TGAT</h3> 
+                    <h3 class="card-header" style="background-color: #F07C1C; color: whitesmoke; border-radius: 0.5em 0.5em 0 0; ">TGAT</h3>
                     <div class="card-body p-0">
                         <h4 class="mt-2 mb-0" id="timer"></h4>
                         <h2>Day</h2>
                     </div>
                 </div>
-                <div class="card text-center" >
+                <div class="card text-center">
                     <h3 class="card-header" style="background-color: #1E51DE; color: whitesmoke; border-radius: 0.5em 0.5em 0 0;">TPAT</h3>
                     <div class="card-body p-0">
                         <h4 class="mt-2 mb-0" id="timer1"></h4>
@@ -27,7 +27,7 @@
                         <h2>Day</h2>
                     </div>
                 </div>
-                <div class="card text-center" >
+                <div class="card text-center">
                     <h3 class="card-header" style="background-color: #C1272D; color: whitesmoke; border-radius: 0.5em 0.5em 0 0;  ">A-Level</h3>
                     <div class="card-body p-0">
                         <h4 class="mt-2 mb-0" id="timer3"></h4>
@@ -39,37 +39,37 @@
         <!-- <div class="m-3 d-flex justify-content-center align-items-center img1">
             <img class="imgtcas" src="img/TCAS.jpg">
         </div> -->
-        <?php
-        // conect database
-        require 'server.php';
-        require 'pagination-v2.class.php';
-        $page = new PaginationV2();
+        <div class="container-sm" style="max-width: 1100px;">
+            <?php
+            // conect database
+            require 'server.php';
+            require 'pagination-v2.class.php';
+            $page = new PaginationV2();
 
-        $sql =  "SELECT * FROM news 
+            $sql =  "SELECT * FROM news 
                 WHERE category='TCAS67' 
                 ORDER BY UploadDate DESC";
-        $result = $page->query($mysqli, $sql, 5);
+            $result = $page->query($mysqli, $sql, 5);
 
-        // get data
-        if ($result) {
-            while ($dbarr = $result->fetch_assoc()) {
-                $id = $dbarr['id'];
-                $topic = $dbarr['topic'];
+            // get data
+            if ($result) {
+                while ($dbarr = $result->fetch_assoc()) {
+                    $id = $dbarr['id'];
+                    $topic = $dbarr['topic'];
 
-                $UploadDate = $dbarr['UploadDate'];
-                // format jesusYear ti buddhishYear
-                $buddhistYear = intval(date('Y', strtotime($UploadDate))) + 543;
-                $buddhistDate = date('d/m/', strtotime($UploadDate)) . $buddhistYear;
+                    $UploadDate = $dbarr['UploadDate'];
+                    // format jesusYear ti buddhishYear
+                    $buddhistYear = intval(date('Y', strtotime($UploadDate))) + 543;
+                    $buddhistDate = date('d/m/', strtotime($UploadDate)) . $buddhistYear;
 
-                $img = $dbarr['img'];
-                $category = $dbarr['category'];
-                echo <<<HTML
+                    $img = $dbarr['img'];
+                    $category = $dbarr['category'];
+                    echo <<<HTML
                     <!-- output -->
-                    <div class="container-sm" style="max-width: 1100px;">
-                        <div class="card mb-3 mx-1">
+                        <div class="card mb-3">
                             <div class="row g-0">
                                 <div class="col-md-6">
-                                    <img src="$img" class="img-fluid rounded-start">
+                                    <img src="$img" style="object-fit: cover; width: 100%; height: 30vh">
                                 </div>
                                 <div class="col-md-6">
                                     <div class="card-body d-flex flex-column justify-content-between" style="height: 100%">
@@ -84,13 +84,13 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
                     HTML;
+                }
             }
-        }
-        require('subpage/pagination.inc.php'); //pagination
-        $mysqli->close();
-        ?>
+            require('subpage/pagination.inc.php'); //pagination
+            $mysqli->close();
+            ?>
+        </div>
     </section>
     <?php require('subpage/footer.inc.php'); //footer
     ?>

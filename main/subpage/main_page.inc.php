f6540506dfa4aa09f3ceedeb2f32ede08dd81c21
BazkilleR <ohm.ratchaphon@gmail.com>
2023-10-17 16:10:29 +0700

    fix small screen news, add category & date

--- main/subpage/main_page.inc.php
+++ main/subpage/main_page.inc.php
@@ -1,7 +1,7 @@
 <section>
     <div class="container-xl py-5">
         <div id="main-news" class="row g-3">
-            <div class="col-12 col-lg-8">
+            <div class="col-12 col-lg-8 mb-5">
                 <div class="row mb-3">
                     <div class="col-12 fs-1 head-title">ข่าวล่าสุด</div>
                 </div>
@@ -19,7 +19,12 @@
 
                     while ($dbarr = $result->fetch_assoc()) {
                         $id = $dbarr['id'];
+
                         $topic = $dbarr['topic'];
+                        if (strlen($topic) > 50) {
+                            $topic = mb_substr($topic, 0, 50) . '...';
+                        }
+
                         $UploadDate = $dbarr['UploadDate'];
 
                         // Format UploadDate to Buddhist calendar
@@ -40,9 +45,12 @@
                                             <a href="show_detail.php?id=<?= $id ?>" class="card-title fs-2 stretched-link text-decoration-none" style="color: white;">
                                                 <?= $topic ?>
                                             </a>
-                                            <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
-                                                <?= $category ?>
-                                            </a>
+                                            <div class="d-flex gap-2">
+                                                <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
+                                                    <?= $category ?>
+                                                </a>
+                                                <p class="mb-0" style="color: white;"><?= $buddhist_date ?></p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -54,18 +62,21 @@
                                 <!-- Other News Articles -->
                                 <div class="col-md-12 small-screen-news">
                                     <div class="card h-100 shadow-sm">
-                                        <div class="row g-0" style="height: 10rem; overflow: hidden;">
+                                        <div class="row g-0">
                                             <div class="col-4">
-                                                <img src="<?= $img ?>" class="card-img w-100 h-100" style=" object-fit: cover;" />
+                                                <img src="<?= $img ?>" class="card-img" style="object-fit: cover; width: 100%; height: 10rem;" />
                                             </div>
                                             <div class="col-8">
-                                                <div class="card-body d-flex flex-column justify-content-end">
+                                                <div class="card-body d-flex flex-column justify-content-between" style="height: 100%">
                                                     <a href="show_detail.php?id=<?= $id ?>" class="card-title stretched-link text-decoration-none">
                                                         <?= $topic ?>
                                                     </a>
-                                                    <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
-                                                        <?= $category ?>
-                                                    </a>
+                                                    <div class="d-flex justify-content-between" style="font-size: .8rem;">
+                                                        <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
+                                                            <?= $category ?>
+                                                        </a>
+                                                        <p class="mb-0"><?= $buddhist_date ?></p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -78,9 +89,12 @@
                                             <a href="show_detail.php?id=<?= $id ?>" class="card-title stretched-link text-decoration-none">
                                                 <?= $topic ?>
                                             </a>
-                                            <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
-                                                <?= $category ?>
-                                            </a>
+                                            <div class="d-flex justify-content-between" style="font-size: .8rem;">
+                                                <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
+                                                    <?= $category ?>
+                                                </a>
+                                                <p class="mb-0"><?= $buddhist_date ?></p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -109,7 +123,12 @@
 
                     while ($dbarr = $result->fetch_assoc()) {
                         $id = $dbarr['id'];
+
                         $topic = $dbarr['topic'];
+                        if (strlen($topic) > 30) {
+                            $topic = mb_substr($topic, 0, 30) . '...';
+                        }
+
                         $UploadDate = $dbarr['UploadDate'];
 
                         // Format UploadDate to Buddhist calendar
@@ -119,8 +138,7 @@
                         $category = $dbarr['category'];
                         $img = $dbarr['img'];
 
-                        if ($count == 0) {
-                ?>
+                        if ($count == 0) { ?>
                             <!-- Important News Article -->
                             <div class="row g-3">
                                 <div class="col-12">
@@ -130,30 +148,35 @@
                                             <a href="show_detail.php?id=<?= $id ?>" class="card-title stretched-link text-decoration-none">
                                                 <?= $topic ?>
                                             </a>
-                                            <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
-                                                <?= $category ?>
-                                            </a>
+                                            <div class="d-flex justify-content-between" style="font-size: .8rem;">
+                                                <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
+                                                    <?= $category ?>
+                                                </a>
+                                                <p class="mb-0"><?= $buddhist_date ?></p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             <?php
-                        } else {
-                            ?>
+                        } else { ?>
                                 <!-- Other Important News Articles -->
-                                <div class="col-12">
-                                    <div class="card w-100 h-100 shadow-sm">
-                                        <div class="row g-0" style="height: 10rem; overflow: hidden;">
+                                <div class="col-md-12">
+                                    <div class="card h-100 shadow-sm">
+                                        <div class="row g-0">
                                             <div class="col-4">
-                                                <img src="<?= $img ?>" class="card-img w-100 h-100" style="height: 10vh; object-fit: cover;" />
+                                                <img src="<?= $img ?>" class="card-img" style="object-fit: cover; width: 100%; height: 10rem;" />
                                             </div>
                                             <div class="col-8">
-                                                <div class="card-body h-100 d-flex flex-column justify-content-between">
+                                                <div class="card-body d-flex flex-column justify-content-between" style="height: 100%">
                                                     <a href="show_detail.php?id=<?= $id ?>" class="card-title stretched-link text-decoration-none">
                                                         <?= $topic ?>
                                                     </a>
-                                                    <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
-                                                        <?= $category ?>
-                                                    </a>
+                                                    <div class="d-flex justify-content-between" style="font-size: .8rem;">
+                                                        <a href="<?= $category ?>_news.php" id="category-text" class="card-title text-decoration-none mb-0" style="color: #ffb7e0;">
+                                                            <?= $category ?>
+                                                        </a>
+                                                        <p class="mb-0"><?= $buddhist_date ?></p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
